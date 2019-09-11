c218d88e3c52d8ea156d5917c090d527448877b1
basitshah94 <basit.shah94@gmail.com>
2019-09-11 17:09:04 +0500

    basit_one_kaleempush

--- app/src/main/java/com/fyp_project/bustracking/MainActivity.java
+++ app/src/main/java/com/fyp_project/bustracking/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+        raja kaleem
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
